4381d7f35abe6b9c944e4f6d4f178656c80d8d5b
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-07-21 23:48:40 +0100

    fix:

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import ListingEditSreen from "./app/screens/ListingEditScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
   return <ListingEditSreen />;
