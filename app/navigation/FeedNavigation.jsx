e9149679fdd0c6d2c2cfd4fcfd0f7deea00a1170
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-07-25 18:57:27 +0100

    Refactor: Routes

--- app/navigation/FeedNavigation.jsx
+++ app/navigation/FeedNavigation.jsx
@@ -3,16 +3,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import ListingsScreen from "../screens/ListingsScreen";
 import ListingDetailsScreen from "../screens/ListingDetailsScreen";
+import routes from "./routes";
 
 const Stack = createStackNavigator();
 
 const FeedNavigator = () => (
-  <Stack.Navigator mode="modal">
-    <Stack.Screen name="Listings" component={ListingsScreen} />
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name={routes.LISTINGS} component={ListingsScreen} />
     <Stack.Screen
-      name="ListingDetails"
+      name={routes.LISTING_DETAILS}
       component={ListingDetailsScreen}
-      //   options={{ headerShown: false }}
     />
   </Stack.Navigator>
 );
---
+++ app/navigation/routes.js
@@ -0,0 +1,10 @@
+export default Object.freeze({
+  ACCOUNT: "Account",
+  FEED: "Feed",
+  WELCOME: "welcome",
+  LOGIN: "Login",
+  REGISTER: "Register",
+  LISTINGS: "Listings",
+  LISTING_DETAILS: "ListingDetails",
+  LISTING_EDIT: "ListingEdit",
+});
