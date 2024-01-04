bc1db693b35200b41648c8a13a8bb21d99415a03
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-01-04 18:38:59 +0100

    feat: did a little thing

--- app/navigation/routes.js
+++ app/navigation/routes.js
@@ -1,10 +1,10 @@
 export default Object.freeze({
   ACCOUNT: "Account",
   FEED: "Feed",
-  WELCOME: "welcome",
   LISTING_EDIT: "ListingEdit",
   LOGIN: "Login",
   REGISTER: "Register",
   LISTINGS: "Listings",
+  WELCOME: "welcome",
   LISTING_DETAILS: "ListingDetails",
 });
