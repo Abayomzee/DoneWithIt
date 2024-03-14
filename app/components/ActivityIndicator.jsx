02c1344811d4869ab827e772ba9036a2b6466e21
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-03-14 12:09:18 +0100

    feat: small change

--- app/components/ActivityIndicator.jsx
+++ app/components/ActivityIndicator.jsx
@@ -9,9 +9,9 @@ function ActivityIndicator({ visible }) {
       style={{
         top: 0,
         width: "100%",
+        height: "100%",
         position: "absolute",
         backgroundColor: "white",
-        height: "100%",
         zIndex: 1,
         opacity: 0.7,
       }}
