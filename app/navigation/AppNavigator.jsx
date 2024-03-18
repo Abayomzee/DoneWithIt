5350c4650b35992ad775d4a35cc03ef2d125e39c
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2024-03-18 18:30:50 +0100

    feat: small change

--- app/navigation/AppNavigator.jsx
+++ app/navigation/AppNavigator.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 
 import * as Permissions from "expo-permissions";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import * as Notifications from "expo-notifications";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import FeedNavigator from "./FeedNavigation";
